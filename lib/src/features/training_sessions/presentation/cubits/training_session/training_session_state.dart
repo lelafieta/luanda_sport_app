f6be45304bf7cc4c8a973ad4b6d1da7bf6265413
Lela Fieta <lelafieta11@gmail.com>
2025-05-27 23:30:51 +0100

    Training cubit state

--- lib/src/features/training_sessions/presentation/cubits/training_session/training_session_state.dart
+++ lib/src/features/training_sessions/presentation/cubits/training_session/training_session_state.dart
@@ -8,3 +8,23 @@ sealed class TrainingSessionState extends Equatable {
 }
 
 final class TrainingSessionInitial extends TrainingSessionState {}
+
+final class TrainingSessionLoading extends TrainingSessionState {}
+
+final class TrainingSessionLoaded extends TrainingSessionState {
+  final List<TrainingSessionEntity> traingSessions;
+
+  const TrainingSessionLoaded({required this.traingSessions});
+
+  @override
+  List<Object> get props => [traingSessions];
+}
+
+final class TrainingSessionFailure extends TrainingSessionState {
+  final String message;
+
+  const TrainingSessionFailure({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
